af00dacc7ccf8261d5ba40b56823665fc088afa4
sakir456 <shaikhsakir1015@gmail.com>
2025-03-26 12:06:15 +0530

    first commit

---
+++ assets/assets.js
@@ -0,0 +1,136 @@
+import user_image from './user-image.jpg';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';                             
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import building from './building.png'
+import shopping_cart from './shopping-cart.png'
+import cloud from './cloud.png'
+import earth_globe from './earth-globe.png'
+import technology from './technology.png'
+import technology_icon_dark from './technology-dark.png'
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    firebase,
+    figma,
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark,
+    building,
+    shopping_cart,
+    cloud,
+    earth_globe,
+    technology,
+    technology_icon_dark
+};
+
+export const workData = [
+    {
+        title: 'FitHub(User) ',
+        description: 'Gym Managment System',
+        bgImage: '/Fithub-User.png',
+        link:'https://fithub-frontend-1bc1.onrender.com/'
+    },
+    {
+        title: 'FitHub(Admin) ',
+        description: 'Gym Managment System',
+        bgImage: '/Fithub-Admin.png',
+        link:'https://fithub-admin.onrender.com/'
+    },
+    {
+        title: 'Prescripto(User) ',
+        description: 'Hospital Managment System',
+        bgImage: '/prescripto.png',
+        link:'https://prscripto-frontend-git-main-sakirs-projects-cdc511d6.vercel.app/'
+    },
+    {
+        title: 'Prescripto(Admin) ',
+        description: 'Hospital Managment System',
+        bgImage: '/Prescripto-Admin.png',
+        link:'https://prescripto-admin-git-main-sakirs-projects-cdc511d6.vercel.app/'
+    },
+    {
+        title: 'SaleSmart',
+        description: 'Invoice Managment System',
+        bgImage: '/SaleSmart.png',
+        link:'https://salesmartfrontend-git-main-sakirs-projects-cdc511d6.vercel.app/'
+    },
+    
+]
+
+export const serviceData = [
+    { icon: assets.earth_globe, title: ' Custom Website Development ', description: ' Build fully responsive and scalable websites tailored to your business needs.', link: '' },
+    { icon: assets.building, title: 'Business Web Applications', description: 'Develop powerful and efficient web apps using MERN stack to streamline operations.', link: '' },
+    { icon: assets.shopping_cart, title: 'E-commerce Development', description: 'UCreate online stores with secure payment gateways (Razorpay, Stripe, PayPal) and inventory management', link: '' },
+    { icon: assets.cloud, title: 'Web App Deployment & Maintenance', description: 'Ensure smooth hosting, deployment, and post-launch support on Vercel, Netlify, or AWS.', link: '' },
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.technology, iconDark: assets.technology_icon_dark, title: 'Technology Stack', description: 'React.js, Next.js, Node.js, Express.js, Tailwind CSS, mongoDB' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+];
+
+export const toolsData = [
+    assets.vscode, assets.firebase, assets.mongodb,assets.git
+];
