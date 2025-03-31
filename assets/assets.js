c619449291c2c100b5c8aa1ae799b9d6b9f6f405
sakir456 <shaikhsakir1015@gmail.com>
2025-03-31 22:16:33 +0530

    refactor: update resume details and improve language listing

--- assets/assets.js
+++ assets/assets.js
@@ -126,7 +126,7 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'JavaScript, HTML, CSS' },
     { icon: assets.technology, iconDark: assets.technology_icon_dark, title: 'Technology Stack', description: 'React.js, Next.js, Node.js, Express.js, Tailwind CSS, mongoDB' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
